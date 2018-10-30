6c4559af19654e244b604edf8e28c71702a011e8
Amy Baldwin <amybaldwindev@gmail.com>
2018-10-29 20:01:10 -0700

    Lecture 29

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -370,14 +370,43 @@
 // 	console.log(i);
 // }
 
-var john = ['John', 'Smith', 1990, 'designer', false];
+// var john = ['John', 'Smith', 1990, 'designer', false];
 
+
+// for loops
 // for (var i = 0; i < john.length; i++) {
 // 	console.log(john[i]);
 // }
 
-var i = 0;
-while (i < john.length) {
-	console.log(john[i]);
-	i++;
-}
+// while loops
+
+// var i = 0;
+// while (i < john.length) {
+// 	console.log(john[i]);
+// 	i++;
+// }
+
+
+// continue and break statements
+// break out of the loop
+
+// var john = ['John', 'Smith', 1990, 'designer', false, 'blue'];
+
+// for (var i = 0; i < john.length; i++) {
+// 	if (typeof john[i] !== 'string') continue;
+// 	console.log(john[i]);
+// }
+
+// for (var i = 0; i < john.length; i++) {
+// 	if (typeof john[i] !== 'string') break;
+// 	console.log(john[i]);
+// }
+
+
+// Coding Challeng 
+// loop through the array in reverse order
+
+// for (var i = john.length -1; i >= 0; i--) {
+// 	console.log(john[i]);
+// }
+
