6f9b5ac237b32e5ee3b94a71929b6ccb9b2b128a
Amy Baldwin <amybaldwindev@gmail.com>
2018-11-10 20:53:56 -0800

    video 39

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -1,16 +1,33 @@
 ///////////////////////////////////////
 // Lecture: Hoisting
+// calculateAge(1995);
+
+// hoisting only works for function decairation
+function calculateAge(year) {
+    console.log(2018 - year)
+}
+
+ // not function expressions as below
+// retirement(1990);
+
+var retirement = function (year) {
+    console.log(65 - (2018 - year));
+}
+
+// variables that don't have a value yet will always be undefinde
+console.log(age);
+var age = 23;
 
 
+function foo() {
+    var age = 65;
+    console.log(age);
+}
+foo();
+console.log(age);
 
-
-
-
-
-
-
-
-
+// use case for hoisting is that we can use function declairations before they
+// are used in the code
 
 
 
