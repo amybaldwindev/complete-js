0ebddb07bf7f179bbd93bf632a9ac815da9e765a
Amy Baldwin <amybaldwindev@gmail.com>
2019-05-27 14:31:37 -0700

    closure notes, lecture 68 & exercise

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -56,104 +56,169 @@ mark.calculateAge();
 //  Primitives vs objects
 
 // primitives
-let a = 23
-let b = a
+// let a = 23
+// let b = a
 
-a =  46
-console.log(a, b); // 46, 23
+// a =  46
+// console.log(a, b); // 46, 23
 
-//  Objects
-var obj1 = {
-	name: 'John',
-	age: 29
-};
+// //  Objects
+// var obj1 = {
+// 	name: 'John',
+// 	age: 29
+// };
 
-var obj2 = obj1;
+// var obj2 = obj1;
 
-obj1.age = 30;
+// obj1.age = 30;
 
-console.log(obj1, obj2); // 30, 30
+// console.log(obj1, obj2); // 30, 30
 
-// Functions
-var age = 25; // copy bc it's  a primitive
-var obj = {
-	name: 'Amy',
-	city: 'Los Angeles' // refernce 
-};
+// // Functions
+// var age = 25; // copy bc it's  a primitive
+// var obj = {
+// 	name: 'Amy',
+// 	city: 'Los Angeles' // refernce 
+// };
 
-function change(a, b) {
-	a = 30;
-	b.city = 'Christchurch';
-}
+// function change(a, b) {
+// 	a = 30;
+// 	b.city = 'Christchurch';
+// }
 
-change(age, obj);
-console.log(age, obj.city)
+// change(age, obj);
+// console.log(age, obj.city)
 
-//  functions are also objects
-//  functions are instance of object type
-//  functions behaves like any other object;
-//  we can store functions in a variable;
-//  we can pass functions as arguements to other functions
-//  we can return a function from a function.
+// //  functions are also objects
+// //  functions are instance of object type
+// //  functions behaves like any other object;
+// //  we can store functions in a variable;
+// //  we can pass functions as arguements to other functions
+// //  we can return a function from a function.
 
-var years = [ 1990, 1965, 1937, 2005, 1998];
+// var years = [ 1990, 1965, 1937, 2005, 1998];
+// var fahrenheit = [40, 60, 80, 100, 110];
 
-function arrayCalc(array, fctn) {
-	var arrRes = [];
-	for (var i = 0; i < array.length; i++){
-		arrRes.push(fctn(array[i]));
-	}
-	return arrRes;
-}
+// function cToF(deg){
+// 	var celsius = [];
+// 	for (i = 0; i < deg.length; i++) {
+// 		celsius.push((Math.round((deg[i]-32)* 5/9)));
+// 	}
+// 	console.log(celsius);
+// }
+// cToF(fahrenheit);
 
-function calculateAge(el) {
-	return 2019 - el;
-}
+// console.log(cTemp());
 
-function isFullAge(el) {
-	return el >= 18;
-}
+// function arrayCalc(array, fctn) {
+// 	var arrRes = [];
+// 	for (var i = 0; i < array.length; i++){
+// 		arrRes.push(fctn(array[i]));
+// 	}
+// 	return arrRes;
+// }
 
-function maxHeartRate(el) {
-	if(el >= 18 && el <= 81){
-		return Math.round(206.9 -(0.67*el));
-	} else {
-		return -1;
-	}	
-}
+// function calculateAge(el) {
+// 	return 2019 - el;
+// }
 
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var maxBPM = arrayCalc(ages, maxHeartRate);
+// function isFullAge(el) {
+// 	return el >= 18;
+// }
 
-console.log(ages);
-console.log(fullAges);
-console.log(maxBPM);
+// function maxHeartRate(el) {
+// 	if(el >= 18 && el <= 81){
+// 		return Math.round(206.9 -(0.67*el));
+// 	} else {
+// 		return -1;
+// 	}	
+// }
 
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
+// var maxBPM = arrayCalc(ages, maxHeartRate);
 
-function interviewQuestion(job) {
-	if (job === 'designer') {
-		return function(name) {
-			console.log(name + ' , can you please explain what UX design is?');
-		}
-	} else if (job === 'teacher') {
-		return function(name) {
-			console.log(name +', which subject do you teach?');
-		}
-	} else {
-		return function(name) {
-			console.log('Hello ' + name + ', what do you do?');
-		}
-	}
-}
-
-var teacherQuestion = interviewQuestion('teacher');
-var designerQuestion = interviewQuestion('designer');
-var engineerQuestion = interviewQuestion('engineer');
-
-teacherQuestion('John');
-designerQuestion('John');
-engineerQuestion('John');
+// console.log(ages);
+// console.log(fullAges);
+// console.log(maxBPM);
 
 
-interviewQuestion('teacher')('Mark');
+// function interviewQuestion(job) {
+// 	if (job === 'designer') {
+// 		return function(name) {
+// 			console.log(name + ' , can you please explain what UX design is?');
+// 		}
+// 	} else if (job === 'teacher') {
+// 		return function(name) {
+// 			console.log(name +', which subject do you teach?');
+// 		}
+// 	} else {
+// 		return function(name) {
+// 			console.log('Hello ' + name + ', what do you do?');
+// 		}
+// 	}
+// }
+
+// var teacherQuestion = interviewQuestion('teacher');
+// var designerQuestion = interviewQuestion('designer');
+// var engineerQuestion = interviewQuestion('engineer');
+
+// teacherQuestion('John');
+// designerQuestion('John');
+// engineerQuestion('John');
+
+
+// interviewQuestion('teacher')('Mark');
+
+// IFFE
+// not for reusable code, for data privacy and code modularity
+
+// function game() {
+// 	var score = Math.random() *10;
+// 	console.log(score >= 5);
+// }
+// game();
+
+// (function(goodLuck) {
+// 	var score = Math.random() *10;
+// 	console.log(score >= 5 - goodLuck);
+// })(5);
+// console.log(score); //hidden
+
+// closures
+
+// function retirement(retirementAge) {
+// 	var a = ' years left until reitrement.';
+// 	return function(yearOfBirth) {
+// 		var age = 2019 - yearOfBirth;
+// 		console.log((retirementAge - age) + a);
+// 	}
+// }
+
+// // retirement(66)(1990);
+// var retirementUs = retirement(66);
+// var retirementEurope = retirement(65);
+// var retirementIceland =  retirement(67);
+// retirementUs(1990);
+// retirementEurope(1990);
+// retirementIceland(1990)
+
+// exerciser re-write interview question with closures.
+
+// function interviewQuestion(job) {
+// 	//note vars defined here would be available in below function.
+// 	return function (name) {
+// 		if(job === 'teacher') {
+// 			console.log(name + ', which subject do you teach?');
+// 		} else if (job === 'designer') {
+// 			console.log(name + ', could you please explain what UX design is?');
+// 		} else {
+// 			console.log('What do you do ' + name + '?');
+// 		}
+// 	}
+// }
+
+// interviewQuestion('teacher')('John');
+// interviewQuestion('designer')('Marc');
+// interviewQuestion('engineer')('Mary');
+
